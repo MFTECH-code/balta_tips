5fd3f1a050b4f427fb9d89c7e738d1317281e503
MFTECH-code <matheussilvafeitosa78@gmail.com>
2022-04-21 12:07:55 -0300

    Lendo arquivos no C#

---
+++ LendoArquivos/Program.cs
@@ -0,0 +1,26 @@
+﻿const string filePath = "D:\\projetos\\files\\text-file.txt";
+
+// File
+// var data = File.ReadAllText(filePath);
+// Console.WriteLine(data);
+
+// ReadAllText lê o aqruivo inteiro
+
+// var data = File.ReadAllLines(filePath);
+// var count = 0;
+// foreach (var line in data)
+// {
+//     count++;
+//     Console.WriteLine($"Linha: {count} - {line}");
+// }
+
+// ReadAllLines lê linha a linha e guarda em um vetor
+
+// StreamReader -> recomendado para arquivos mais pesados, pois ele não lê tudo de uma só vez
+using var file = new StreamReader(filePath);
+string? line;
+while ((line = file.ReadLine()) != null)
+    Console.WriteLine(line);
+
+
+file.Close();
